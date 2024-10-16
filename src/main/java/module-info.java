cc231f06bda6b84aa066f3e4c9c6954f3c64b206
Manuel Rodriguez <juan.rodriguez.o@correounivalle.edu.co>
2024-10-15 19:42:11 -0500

    Base para arquitectura MVC, layout inicial para el juego y patron singleton para el GameStage

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,6 @@ module com.example.proyecto2fpoe {
 
 
     opens com.example.proyecto2fpoe to javafx.fxml;
+    opens com.example.proyecto2fpoe.Controller to javafx.fxml;
     exports com.example.proyecto2fpoe;
 }
